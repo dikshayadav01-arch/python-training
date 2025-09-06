5e8548c53490bae1b3900aaead5a1230691803fb
dikshayadav01-arch <dyadavofficial01@gmail.com>
2025-09-06 19:06:01 +0530

    assignment 1

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+add=a+b
+print("addition: ",add)
+sub=a-b
+print("subtraction: ",sub)
+mul=a*b
+print("multiplication: ",mul)
+div=a/b
+print("division: ",div)
